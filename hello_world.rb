c2596bbc9834754bfe0689fe56c0f97f0f3a106b
Laura Mashburn <lmashburn@gapintelligence.com>
2017-03-20 17:10:45 -0700

    homework from class 3

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,12 @@
-puts 'it takes ten-tickles to make an octopus laugh'
+class HelloWorld
+	def initialize(string_to_output)
+		@string_to_output = string_to_output
+	end
+
+	def say_something
+		puts @string_to_output
+	end
+end
+
+my_hello_world = HelloWorld.new("it takes ten tickles to make an octopus laugh")
+my_hello_world.say_something
